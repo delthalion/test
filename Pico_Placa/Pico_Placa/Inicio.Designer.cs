da39ee926789230460b2991550ce32c696035255
delthalion <57551393+delthalion@users.noreply.github.com>
2019-11-10 08:44:07 -0500

    Add files via upload

---
+++ Pico_Placa/Pico_Placa/Inicio.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Pico_Placa
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BTN_VERIFICAR = new System.Windows.Forms.Button();
+            this.GB_INFORMACION = new System.Windows.Forms.GroupBox();
+            this.LBL_HORA = new System.Windows.Forms.Label();
+            this.LBL_FECHA = new System.Windows.Forms.Label();
+            this.LBL_PLACA = new System.Windows.Forms.Label();
+            this.TB_HORA = new System.Windows.Forms.TextBox();
+            this.TB_FECHA = new System.Windows.Forms.TextBox();
+            this.TB_PLACA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TB_RESPUESTA = new System.Windows.Forms.TextBox();
+            this.BTN_NUEVA_BUSQ = new System.Windows.Forms.Button();
+            this.GB_INFORMACION.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BTN_VERIFICAR
+            // 
+            this.BTN_VERIFICAR.Location = new System.Drawing.Point(34, 235);
+            this.BTN_VERIFICAR.Name = "BTN_VERIFICAR";
+            this.BTN_VERIFICAR.Size = new System.Drawing.Size(136, 37);
+            this.BTN_VERIFICAR.TabIndex = 0;
+            this.BTN_VERIFICAR.Text = "VERIFICAR";
+            this.BTN_VERIFICAR.UseVisualStyleBackColor = true;
+            this.BTN_VERIFICAR.Click += new System.EventHandler(this.BTN_VERIFICAR_Click);
+            // 
+            // GB_INFORMACION
+            // 
+            this.GB_INFORMACION.Controls.Add(this.LBL_HORA);
+            this.GB_INFORMACION.Controls.Add(this.LBL_FECHA);
+            this.GB_INFORMACION.Controls.Add(this.LBL_PLACA);
+            this.GB_INFORMACION.Controls.Add(this.TB_HORA);
+            this.GB_INFORMACION.Controls.Add(this.TB_FECHA);
+            this.GB_INFORMACION.Controls.Add(this.TB_PLACA);
+            this.GB_INFORMACION.Location = new System.Drawing.Point(22, 62);
+            this.GB_INFORMACION.Name = "GB_INFORMACION";
+            this.GB_INFORMACION.Size = new System.Drawing.Size(489, 152);
+            this.GB_INFORMACION.TabIndex = 1;
+            this.GB_INFORMACION.TabStop = false;
+            this.GB_INFORMACION.Text = "INFORMACIÓN:";
+            // 
+            // LBL_HORA
+            // 
+            this.LBL_HORA.AutoSize = true;
+            this.LBL_HORA.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.LBL_HORA.Location = new System.Drawing.Point(164, 108);
+            this.LBL_HORA.Name = "LBL_HORA";
+            this.LBL_HORA.Size = new System.Drawing.Size(311, 13);
+            this.LBL_HORA.TabIndex = 8;
+            this.LBL_HORA.Text = "INGRESAR LA HORA (HH:MM) EJ: 08:45 o 15:25 (24 HORAS) ";
+            // 
+            // LBL_FECHA
+            // 
+            this.LBL_FECHA.AutoSize = true;
+            this.LBL_FECHA.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.LBL_FECHA.Location = new System.Drawing.Point(164, 66);
+            this.LBL_FECHA.Name = "LBL_FECHA";
+            this.LBL_FECHA.Size = new System.Drawing.Size(280, 13);
+            this.LBL_FECHA.TabIndex = 7;
+            this.LBL_FECHA.Text = "INGRESAR LA FECHA (DD/MM/AAAA). EJ: 11/11/2019";
+            // 
+            // LBL_PLACA
+            // 
+            this.LBL_PLACA.AutoSize = true;
+            this.LBL_PLACA.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.LBL_PLACA.Location = new System.Drawing.Point(164, 31);
+            this.LBL_PLACA.Name = "LBL_PLACA";
+            this.LBL_PLACA.Size = new System.Drawing.Size(245, 13);
+            this.LBL_PLACA.TabIndex = 6;
+            this.LBL_PLACA.Text = "INGRESAR LA PLACA (XXX-####). EJ: PIB-1904";
+            // 
+            // TB_HORA
+            // 
+            this.TB_HORA.Enabled = false;
+            this.TB_HORA.Location = new System.Drawing.Point(21, 108);
+            this.TB_HORA.Name = "TB_HORA";
+            this.TB_HORA.Size = new System.Drawing.Size(115, 20);
+            this.TB_HORA.TabIndex = 5;
+            this.TB_HORA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TB_HORA_CheckKeys);
+            // 
+            // TB_FECHA
+            // 
+            this.TB_FECHA.Enabled = false;
+            this.TB_FECHA.Location = new System.Drawing.Point(21, 66);
+            this.TB_FECHA.Name = "TB_FECHA";
+            this.TB_FECHA.Size = new System.Drawing.Size(115, 20);
+            this.TB_FECHA.TabIndex = 4;
+            this.TB_FECHA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TB_FECHA_CheckKeys);
+            // 
+            // TB_PLACA
+            // 
+            this.TB_PLACA.AcceptsReturn = true;
+            this.TB_PLACA.Location = new System.Drawing.Point(21, 28);
+            this.TB_PLACA.Name = "TB_PLACA";
+            this.TB_PLACA.Size = new System.Drawing.Size(115, 20);
+            this.TB_PLACA.TabIndex = 3;
+            this.TB_PLACA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TB_PLACA_CheckKeys);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(145, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "VERIFIQUE SI SU VEHIICULO PUEDE CIRCULAR...";
+            // 
+            // TB_RESPUESTA
+            // 
+            this.TB_RESPUESTA.Location = new System.Drawing.Point(118, 298);
+            this.TB_RESPUESTA.Name = "TB_RESPUESTA";
+            this.TB_RESPUESTA.Size = new System.Drawing.Size(262, 20);
+            this.TB_RESPUESTA.TabIndex = 9;
+            this.TB_RESPUESTA.Visible = false;
+            // 
+            // BTN_NUEVA_BUSQ
+            // 
+            this.BTN_NUEVA_BUSQ.Location = new System.Drawing.Point(330, 235);
+            this.BTN_NUEVA_BUSQ.Name = "BTN_NUEVA_BUSQ";
+            this.BTN_NUEVA_BUSQ.Size = new System.Drawing.Size(136, 37);
+            this.BTN_NUEVA_BUSQ.TabIndex = 10;
+            this.BTN_NUEVA_BUSQ.Text = "NUEVA BUSQUEDA";
+            this.BTN_NUEVA_BUSQ.UseVisualStyleBackColor = true;
+            this.BTN_NUEVA_BUSQ.Visible = false;
+            this.BTN_NUEVA_BUSQ.Click += new System.EventHandler(this.BTN_NUEVA_BUSQ_Click);
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 356);
+            this.Controls.Add(this.BTN_NUEVA_BUSQ);
+            this.Controls.Add(this.TB_RESPUESTA);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GB_INFORMACION);
+            this.Controls.Add(this.BTN_VERIFICAR);
+            this.Name = "Inicio";
+            this.Text = "PICO & PLACA";
+            this.GB_INFORMACION.ResumeLayout(false);
+            this.GB_INFORMACION.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BTN_VERIFICAR;
+        private System.Windows.Forms.GroupBox GB_INFORMACION;
+        private System.Windows.Forms.TextBox TB_HORA;
+        private System.Windows.Forms.TextBox TB_FECHA;
+        private System.Windows.Forms.TextBox TB_PLACA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LBL_HORA;
+        private System.Windows.Forms.Label LBL_FECHA;
+        private System.Windows.Forms.Label LBL_PLACA;
+        private System.Windows.Forms.TextBox TB_RESPUESTA;
+        public string error_placa= "Error al ingresar la placa, revise el formato." ;
+        public string error_fecha = "Error al ingresar la fecha, revise el formato.";
+        public string error_hora = "Error al ingresar la hora, revise el formato.";
+        public string puede = "SI PUEDE CIRCULAR SU VEHICULO.";
+        public string no_puede = "NO PUEDE CIRCULAR SU VEHICULO.";
+        private System.Windows.Forms.Button BTN_NUEVA_BUSQ;
+    }
+}
+
---
+++ Pico_Placa/Pico_Placa/Inicio.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pico_Placa
+{
+    
+    public partial class Inicio : Form
+    {
+        
+        public string placa, placa_t, fecha, hora, hora_1, min, dia,mes,año;
+        
+        //boton busqueda nueva y limpieza variables
+        private void BTN_NUEVA_BUSQ_Click(object sender, EventArgs e)
+        {
+            TB_PLACA.Clear();
+            TB_PLACA.Enabled = true;
+            LBL_PLACA.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            TB_FECHA.Clear();
+            LBL_FECHA.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            TB_HORA.Clear();
+            LBL_HORA.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            TB_RESPUESTA.Clear();
+            TB_RESPUESTA.Visible = false;
+            BTN_NUEVA_BUSQ.Visible = false;
+            placa = "";
+            placa_t = "";
+            fecha = "";
+            hora = "";
+            hora_1 = "";
+            min = "";
+            dia = "";
+            mes = "";
+            año = "";
+
+        }
+
+        public Inicio()
+        {
+            InitializeComponent();
+            TB_PLACA.Focus(); 
+            
+        }
+
+        //verificacion de datos ingresados de placa.
+        private void TB_PLACA_CheckKeys(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                int temp = 0;
+                placa = TB_PLACA.ToString();
+                if (placa.Contains('-'))
+                {
+                    int posicion = placa.IndexOf("-");
+                    placa_t = placa.Substring(posicion + 1);
+                    if (int.TryParse(placa_t, out temp) && (placa_t.Length == 4))
+                    {
+                        TB_PLACA.Enabled = false;
+                        TB_FECHA.Enabled = true;
+                        TB_FECHA.Focus();
+                        
+                    }
+                    else
+                    {
+                        Mensaje_error(error_placa);
+                        LBL_PLACA.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+                        TB_PLACA.Clear();
+                    }
+                }
+                else
+                {
+                    Mensaje_error(error_placa);
+                    LBL_PLACA.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+                    TB_PLACA.Clear();
+                }
+            }
+        }
+        
+        //verificacion de datos ingresados de fecha.
+        private void TB_FECHA_CheckKeys(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                int temp = 0, temp1=0 , temp2=0 , car_b=0;
+                fecha = TB_FECHA.Text;
+                car_b = fecha.Split('/').Length - 1;
+
+                if ((fecha.Length == 10) && (car_b==2))
+                {
+                    dia = fecha.Substring(0, 2);
+                    mes = fecha.Substring(3, 2);
+                    año = fecha.Substring(6, 4);
+
+                    if (int.TryParse(dia, out temp) && int.TryParse(mes, out temp1) && int.TryParse(año, out temp2) && (Convert.ToInt32(año) >= DateTime.Now.Year))
+                    {
+                        if ((Convert.ToInt32(año) % 4 == 0) && (Convert.ToInt32(año) != 0))
+                        {
+                            if ((Convert.ToInt32(mes) == 2) && (Convert.ToInt32(dia) < 30))
+                            {
+                                TB_FECHA.Enabled = false;
+                                TB_HORA.Enabled = true;
+                                TB_HORA.Clear();
+                                TB_HORA.Focus();
+                                
+                            }
+                            else if (Convert.ToInt32(mes) != 2)
+                            {
+                                TB_FECHA.Enabled = false;
+                                TB_HORA.Enabled = true;
+                                TB_HORA.Clear();
+                                TB_HORA.Focus();
+                                
+                            }
+                            else {
+                                Mensaje_error(error_fecha);
+                                LBL_FECHA.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+                                TB_FECHA.Clear();
+                                fecha = "";
+                            }
+                        }
+                        else {
+                            TB_FECHA.Enabled = false;
+                            TB_HORA.Enabled = true;
+                            TB_HORA.Clear();
+                            TB_HORA.Focus();
+                            
+                        }
+
+                    }
+                    else
+                    {
+                        Mensaje_error(error_fecha);
+                        LBL_FECHA.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+                        TB_FECHA.Clear();
+                        fecha = "";
+                    }
+                                 
+                }
+                else
+                {
+                    Mensaje_error(error_fecha);
+                    LBL_FECHA.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+                    TB_FECHA.Clear();
+                    fecha = "";
+                }
+            }
+                   
+        }
+       
+        //verificacion de datos ingresados de hora.
+        private void TB_HORA_CheckKeys(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                int temp = 0, temp1 = 0, car_b = 0;
+                hora = TB_HORA.Text;
+                car_b = hora.Split(':').Length - 1;
+
+                if ((hora.Length == 5) && (car_b == 1))
+                {
+                    hora_1 = hora.Substring(0, 2);
+                    min = hora.Substring(3, 2);
+                    
+                    if (int.TryParse(hora_1, out temp) && int.TryParse(min, out temp1) && (Convert.ToInt32(hora_1) < 25) && (Convert.ToInt32(min) < 60))
+                    {
+                        TB_HORA.Enabled = false;
+                       // BTN_VERIFICAR.Focus();
+                        BTN_VERIFICAR.Enabled = true;
+                    }
+                    else
+                    {
+                        Mensaje_error(error_hora);
+                        LBL_HORA.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+                        TB_HORA.Clear();
+                        hora = "";
+                    }
+
+                }
+                else
+                {
+                    Mensaje_error(error_hora);
+                    LBL_HORA.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+                    TB_HORA.Clear();
+                    hora = "";
+                }
+            }
+
+        }
+
+        /*busqueda de circulacion de acuerdo a las reglas:
+          terminacion de placas en:
+                1 y 2 lunes 
+                3 y 4 martes            restriccion con horarios: 
+                5 y 6 miercoles          07:00 hasta 09:30      
+                7 y 8 jueves             16:00 hasta 19:30  
+                9 y 0 viernes 
+         */
+        private void BTN_VERIFICAR_Click(object sender, EventArgs e)
+        {
+            DateTime dia_Semana;
+            string dsemana,rhora;
+            dia_Semana = Convert.ToDateTime(fecha);
+            dsemana = (dia_Semana.ToString("dddd"));
+            int horar, digito;
+            digito = Convert.ToInt32(placa_t.Substring(3));
+            rhora = hora.Replace(":", "");
+            horar = Convert.ToInt32(rhora);
+            
+            if (dsemana == "lunes")
+            {
+                if (digito == 1 || digito == 2)
+                {
+                    if ((horar < 0700) || (horar > 0930 && horar < 1600) || (horar > 1930))
+                    {
+                        Mensaje_circular(puede); 
+                    }
+                    else
+                    {
+                        Mensaje_circular(no_puede); 
+                    }
+                }
+                else
+                {
+                    Mensaje_circular(puede);
+                }
+            }
+            else if (dsemana == "martes")
+            {
+                if (digito == 3 || digito == 4)
+                {
+
+                    if ((horar < 0700) || (horar > 0930 && horar < 1600) || (horar > 1930))
+                    {
+                        Mensaje_circular(puede); 
+                    }
+                    else
+                    {
+                        Mensaje_circular(no_puede);
+                    }
+                }
+                else
+                {
+                    Mensaje_circular(puede);
+                }
+            }
+            else if (dsemana == "miércoles")
+            {
+                if (digito == 5 || digito == 6)
+                {
+                    if ((horar < 0700) || (horar > 0930 && horar < 1600) || (horar > 1930))
+                    {
+                        Mensaje_circular(puede);
+                    }
+                    else
+                    {
+                        Mensaje_circular(no_puede);
+                    }
+                }
+                else
+                {
+                    Mensaje_circular(puede);
+                }
+            }
+            else if (dsemana == "jueves")
+            {
+                if (digito == 7 || digito == 8)
+                {
+                    if ((horar < 0700) || (horar > 0930 && horar < 1600) || (horar > 1930))
+                    {
+                        Mensaje_circular(puede);
+                    }
+                    else
+                    {
+                        Mensaje_circular(no_puede);
+                    }
+                }
+                else
+                {
+                    Mensaje_circular(puede);
+                }
+            }
+            else if (dsemana == "viernes")
+            {
+                if (digito == 9 || digito == 0)
+                {
+                    if ((horar < 0700) || (horar > 0930 && horar < 1600) || (horar > 1930))
+                    {
+                        Mensaje_circular(puede);
+                    }
+                    else
+                    {
+                        Mensaje_circular(no_puede);
+                    }
+                }
+                else
+                {
+                    Mensaje_circular(puede);
+                }
+            }
+            else
+            {
+                Mensaje_circular(puede);
+            }
+            BTN_VERIFICAR.Enabled=false;
+            BTN_VERIFICAR.Visible = false;
+            BTN_NUEVA_BUSQ.Enabled = true;
+            BTN_NUEVA_BUSQ.Visible = true;
+
+        }
+        public void Mensaje_error (string error) {
+            MessageBox.Show(error);
+        }
+        public void Mensaje_circular(string mensaje) {
+            TB_RESPUESTA.Visible = true;
+            TB_RESPUESTA.Enabled = false;
+            TB_RESPUESTA.Text = mensaje;
+        }
+
+    }
+}
